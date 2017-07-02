14deea537c3232d3245d2996db0249bd1e9ec5a7
John Dewey <john@dewey.ws>
2017-07-01 17:41:50 -0700

    Generate asserts tasks from dummy SC model
    Will generate asserts from stockclerk model and pylint the result.

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Ansible Stockclerk
 
 A  POC to validate Ansible inventory.
 
+We generate a set of asserts based off the StockClerk model.  Ansible is
+executed with a `--tags` flag, and the path to the playbook.  Ansible will
+load all roles, thus having variables in scope, so that they can be asserted
+for proper type.
+
+This process is unable to identify undefined variables referenced throughout.
+
 Usage
 -----
 
---
+++ generate.py
@@ -0,0 +1,85 @@
+#  Copyright (c) 2017 Cisco Systems, Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+
+import contextlib
+
+import yaml
+
+
+class SafeDumper(yaml.SafeDumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(SafeDumper, self).increase_indent(flow, False)
+
+
+def safe_load(string):
+    return yaml.safe_load(string) or {}
+
+
+def safe_load_file(filename):
+    with open_file(filename) as stream:
+        return safe_load(stream)
+
+
+def safe_dump(data):
+    return yaml.dump(
+        data, Dumper=SafeDumper, default_flow_style=False, explicit_start=True)
+
+
+def write_file(filename, content):
+    with open_file(filename, 'w') as f:
+        f.write(content)
+
+
+@contextlib.contextmanager
+def open_file(filename, mode='r'):
+    with open(filename, mode) as stream:
+        yield stream
+
+
+def get_assert(var, var_data):
+    if var_data['type'] == 'bool':
+        return _bool_assert(var)
+
+
+def _defined_assert(var):
+    is_defined = '{} is defined'.format(var)
+
+    return {'assert': {'that': is_defined}}
+
+
+def _bool_assert(var):
+    is_bool = '{} | is_bool'.format(var)
+    msg = '"{}" must be of type bool'.format(var)
+
+    return {
+        'assert': {
+            'that': is_bool,
+            'msg': msg,
+        },
+        'tags': ['assert'],
+    }
+
+
+model_dict = safe_load_file('model/model.yml')
+data = [
+    get_assert(var, var_data)
+    for var, var_data in model_dict['groupvars']['all'].items()
+]
+write_file('playbooks/assert_generated.yml', safe_dump(data))
